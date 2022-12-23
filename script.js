571637d1c33b4f0519f41757134cb3b1be417153
Abdurahman-Fahmi <114235665+Abdurahman-Fahmi@users.noreply.github.com>
2022-12-24 00:36:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let startDate = new Date();
+startDate = document.getElementById("pick_date");
+startTime = document.getElementById("pick_time");
+
+startDate.setAttribute("min", new Date().toLocaleDateString("en-ca"));
+startTime.setAttribute("min", new Date().toLocaleDateString("en-ca"));
+
+
+let endDate = new Date();
+endDate = document.getElementById("return_date");
+
+
+
+
+function onSetStartDate() {
+    endDate.setAttribute("min", );
+    alert("as")
+
+}
